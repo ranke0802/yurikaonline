9d0cef1704f696270af965bce4cf7df9b5083013
KIMHANSOO <hi35sojin@metabuild.co.kr>
2026-04-06 10:23:28 +0900

    버전 0.01.68 동기화

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 import Logger from './utils/Logger.js';
-window.GAME_VERSION = '0.01.67'; // Synced with version.txt
+window.GAME_VERSION = '0.01.69'; // Synced with version.txt
 import GameLoop from './core/GameLoop.js';
 import InputManager from './core/InputManager.js';
 import TouchHandler from './core/input/TouchHandler.js';
