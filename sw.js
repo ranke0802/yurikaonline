05be5c1eac84442f718c50429a196a85898bda2c
KIMHANSOO <hi35sojin@metabuild.co.kr>
2026-07-15 16:17:34 +0900

    보스 몬스터 방어력 0 적용

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '0.02.049';
+const APP_VERSION = '0.02.050';
 const SHELL_CACHE = `yurika-online-shell-${APP_VERSION}`;
 const STATIC_CACHE = `yurika-online-static-${APP_VERSION}`;
 const ACTIVE_CACHES = [SHELL_CACHE, STATIC_CACHE];
